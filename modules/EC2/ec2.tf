d954255c57d5141c56b72c0e62555cec3b05c8c8
Khurram Khoja <108364751+khurram539@users.noreply.github.com>
2023-04-06 23:40:55 -0400

    Add files via upload

--- modules/EC2/ec2.tf
+++ modules/EC2/ec2.tf
@@ -1,15 +1,15 @@
 variable "ec2name" {
-    type = string
-}  
+    type = string  
+}
 
-resource "aws_instane" "ec2" {
-    ami = "ami-06e46074ae430fba6"
-    instance_type = "t2.micro"
-    tags = {
-        Name = var.ec2name
-    }
-}     
+resource "aws_instance" "ec2" {
+  ami = "ami-005f9685cb30f234b"
+  instance_type = "t2.micro" 
+  tags = {
+    Name = var.ec2name
+  }
+}
 
 output "instance_id" {
-    value = aws_instance.ec2.id
+  value = aws_instance.ec2.id
 }
--- modules/main.tf
+++ modules/main.tf
@@ -1,12 +1,12 @@
 provider "aws" {
-    region = "us-east-1"
+  region = "us-east-1"
 }
 
 module "ec2module" {
-    source = "./EC2"  
-    ec2name = "Name From Module"
+    source = "./ec2"
+    ec2name = "Name From Modules"
 }
 
-output "module_output" {
-    value = module.ec2module.instance_id  
+output "module_instance_id" {
+  value = module.ec2module.instance_id
 }
