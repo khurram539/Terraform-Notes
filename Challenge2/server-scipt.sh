dc94eb0b1a421f14c924cc1970050ccd8a60efda
Khurram Khoja <108364751+khurram539@users.noreply.github.com>
2023-04-06 23:41:10 -0400

    Delete Challenge2 directory

--- Challenge2/server-scipt.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sudo yum update -y
-sudo yum install httpd -y
-sudo systemctl start httpd
-sudo systemctl enable httpd
-echo "<h1>Hello from Terraform</h1>" | sudo tee /var/www/html/index.html
-
