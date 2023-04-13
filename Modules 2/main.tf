73dec5ccb4b4373750776e0614c3f47a5dbd0158
Khurram Khoja <108364751+khurram539@users.noreply.github.com>
2023-04-13 14:28:36 -0400

    DB

---
+++ Modules 2/main.tf
@@ -0,0 +1,31 @@
+provisioner "aws" {
+    region = "us-east-1"
+
+}
+
+
+
+resource "aws_instance" "webserver" {
+    ami = "ami-0c55b159cbfafe1f0"
+    instance_type = "t2.micro"
+    key_name = "mykey"
+    tags = {
+        Name = "webserver"
+    }
+    provisioner "remote-exec" {
+        inline = [
+            "sudo apt-get update",
+            "sudo apt-get install -y apache2"
+        ]
+    }
+    provisioner "file" {
+        source = "index.html"
+        destination = "/var/www/html/index.html"
+    }
+}
+
+module "dbserver" {
+    source = "./db"
+    dbname = "mydbserver"
+    
+}
