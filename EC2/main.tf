95b99685fb86e5a9a39b9108a33a73be94aaca89
Your Name <khurram539@hotmail.com>
2023-09-18 18:56:08 +0000

    Updated Terraform

--- EC2/main.tf
+++ EC2/main.tf
@@ -1,8 +1,28 @@
-provider "aws" {
-  region = "us-east-1"
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.17.0"
+    }
+  }
 }
-resource "aws_instance" "ec2" {
-  ami = "ami-005f9685cb30f234b"
-  instance_type = "t2.micro" 
-}      
 
+provider "aws" {
+  # Configuration options
+}
+
+resource "aws_instance" "DevOps2" {
+  ami                    = "ami-0b7e8ccd689a48950"
+  instance_type          = "t3.medium"
+  key_name               = "Khurram-key"
+  monitoring             = true
+  vpc_security_group_ids = ["sg-025028548d0e7a3d0"]
+  associate_public_ip_address = true
+  subnet_id              = "subnet-08d90b90e9b121c7e"
+
+
+
+  tags = {
+    Name = "DevOps2"
+  }
+}
