c54c56fb58081dc48da3d54f22ab8b11cf4ff96e
Your Name <khurram539@hotmail.com>
2024-01-03 21:41:06 +0000

    updated EC2 with new AMI

--- EC2/main.tf
+++ EC2/main.tf
@@ -12,7 +12,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "DevBox" {
-  ami                    = "ami-01fdea84725799db3"
+  ami                    = "ami-08c641c912159e1fd"
   instance_type          = "t3a.medium"
   key_name               = "Khurram-key"
   monitoring             = true
