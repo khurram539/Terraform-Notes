f0fedd384b9a7c9488c64cd1f8d66bb0ff05717d
Your Name <khurram539@hotmail.com>
2024-05-01 03:44:03 -0400

    Create new AMI from DevBox

--- AMI/main.tf
+++ AMI/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "5.17.0"
+      version = "5.47.0"
     }
   }
 }
@@ -11,8 +11,7 @@ provider "aws" {
   # Configuration options
 }
 
-resource "aws_ami_from_instance" "example" {
-  name               = "Gold_Image 04/04/2024"
-  source_instance_id = "i-000000000000000"
+resource "aws_ami_from_instance" "New_AMI" {
+  name               = "Gold_Image 05/01/2024"
+  source_instance_id = "i-0e5f7e419d2c08ab6"
 }
-
