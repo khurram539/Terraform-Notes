287d35416b8346f02d453f5f45354d31ce822aad
root <root@RAZER16>
2024-08-16 15:00:42 -0400

    Asible Server

---
+++ Ansible-Server/ansible-server.tf
@@ -0,0 +1,38 @@
+provider "aws" {
+  region = "us-east-1"  # Change to your preferred region
+}
+
+resource "aws_instance" "ansible_server" {
+  ami                         = "ami-02a506f01fced5cc4"  # Amazon Linux 2 AMI ID for us-east-1
+  instance_type               = "t2.small"
+  vpc_security_group_ids      = ["sg-025028548d0e7a3d0"]  # Add your security group ID
+  subnet_id                   = "subnet-08d90b90e9b121c7e"  # Add your subnet ID
+  associate_public_ip_address = true
+  disable_api_termination     = true
+  monitoring                  = true
+  ebs_optimized               = false
+  count                       = 1
+  
+
+  tags = {
+    Name = "AnsibleServer"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum update -y",
+      "sudo amazon-linux-extras install ansible2 -y"
+    ]
+
+    connection {
+      type        = "ssh"
+      user        = "ec2-user"
+      private_key = file("/root/Khurram-key.pem")  # Change to the path of your private key
+      host        = self.public_ip
+    }
+  }
+}
+
+output "ansible_server_ip" {
+  value = aws_instance.ansible_server[0].public_ip
+}
