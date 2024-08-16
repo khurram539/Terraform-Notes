39a546b34c915d3342587d2eb4a8c2d65738ca53
root <root@RAZER16>
2024-08-16 18:07:39 -0400

    Indent Comment spacing

--- Ansible-Server/ansible-server.tf
+++ Ansible-Server/ansible-server.tf
@@ -7,12 +7,12 @@ resource "aws_instance" "ansible_server" {
   key_name                    = "name.pem"                 # Add your key pair name
     vpc_security_group_ids    = ["sg-025028548d0e7a3d0"]   # Add your security group ID
     subnet_id                 = "subnet-08d90b90e9b121c7e" # Add your subnet ID
-  availability_zone          =  "us-east-1a" # Add your availability zone
-  associate_public_ip_address = true  # Assign a public IP address
-  disable_api_termination     = true  # Prevent accidental termination
-  monitoring                  = true  # Enable detailed monitoring
-  ebs_optimized               = false # Disable EBS optimization
-  count                       = 1     # Create a single instance
+  availability_zone          =  "us-east-1a"               # Add your availability zone
+  associate_public_ip_address = true                       # Assign a public IP address
+  disable_api_termination     = true                       # Prevent accidental termination
+  monitoring                  = true                       # Enable detailed monitoring
+  ebs_optimized               = false                      # Disable EBS optimization
+  count                       = 1                          # Create a single instance
 
   tags = {
     Name = "AnsibleServer"
@@ -21,7 +21,7 @@ resource "aws_instance" "ansible_server" {
   root_block_device {
     volume_size = 30 
     volume_type = "gp2" 
-    encrypted   = false # Unencrypt the root volume
+    encrypted   = false                   # Unencrypt the root volume
   }
 
   connection {
@@ -32,22 +32,22 @@ resource "aws_instance" "ansible_server" {
   }
 
   provisioner "file" {
-    source      = "/root/Khurram-key.pem"  # Path to your local PEM key
+    source      = "/root/Khurram-key.pem"    # Path to your local PEM key
     destination = "/home/ec2-user/name.pem"  # Destination path on the remote instance
   }
 
   provisioner "remote-exec" {
     inline = [
-      "sudo yum update -y", # Update the instance
+      "sudo yum update -y",                           # Update the instance
       "sudo amazon-linux-extras install ansible2 -y", # Install Ansible
-      "sudo yum install -y git python3 python3-pip", # Install Git, Python3, and Pip3
-      "sudo pip3 install boto boto3 awscli", # Install Boto, Boto3, and AWS CLI
-      "sudo chown ec2-user:ec2-user /home/ec2-user/name.pem", # Change ownership of the PEM key
-      "sudo chmod 400 /home/ec2-user/name.pem", # Change permissions of the PEM key
+      "sudo yum install -y git python3 python3-pip",  # Install Git, Python3, and Pip3
+      "sudo pip3 install boto boto3 awscli",                   # Install Boto, Boto3, and AWS CLI
+      "sudo chown ec2-user:ec2-user /home/ec2-user/name.pem",  # Change ownership of the PEM key
+      "sudo chmod 400 /home/ec2-user/name.pem",                # Change permissions of the PEM key
       "ssh-keygen -t rsa -N '' -f /home/ec2-user/.ssh/id_rsa", # Generate SSH key pair
       "cat /home/ec2-user/.ssh/id_rsa.pub >> /home/ec2-user/.ssh/authorized_keys", # Add public key to authorized_keys
-      "chmod 600 /home/ec2-user/.ssh/authorized_keys", # Change permissions of authorized_keys
-      "chmod 700 /home/ec2-user/.ssh" # Change permissions of .ssh directory
+      "chmod 600 /home/ec2-user/.ssh/authorized_keys",                             # Change permissions of authorized_keys
+      "chmod 700 /home/ec2-user/.ssh"                                              # Change permissions of .ssh directory
     ]
   }
 }
