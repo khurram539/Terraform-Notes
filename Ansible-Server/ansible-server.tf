3d3a150985ac533347ff361ddf41484f9eeb5240
root <root@RAZER16>
2024-08-16 17:28:56 -0400

    remove ' on line 26

--- Ansible-Server/ansible-server.tf
+++ Ansible-Server/ansible-server.tf
@@ -23,7 +23,7 @@ resource "aws_instance" "ansible_server" {
       "sudo yum update -y",
       "sudo amazon-linux-extras install ansible2 -y",
       "sudo yum install git -y",
-      "'sudo yum install python3 -y",
+      "sudo yum install python3 -y",
       "sudo yum install python3-pip -y",
       "sudo pip3 install boto boto3",
       "sudo pip3 install awscli"
