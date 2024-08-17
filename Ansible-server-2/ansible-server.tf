e2a4440cbc97959ebc06de062f4c7767ee2d6e81
root <root@RAZER16>
2024-08-16 22:40:59 -0400

    Update AMI

--- Ansible-server-2/ansible-server.tf
+++ Ansible-server-2/ansible-server.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"  # Change to your preferred region
 }
 resource "aws_instance" "ansible_server" {
-  ami                         = "ami-02a506f01fced5cc4"    # Amazon Linux 2 AMI ID for us-east-1
+  ami                         = "ami-0ae8f15ae66fe8cda"    # Amazon Linux 2 AMI ID for us-east-1
   instance_type               = "t2.small"                 # Preferred instance type
   key_name                    = "Khurram-key"              # Add your key pair name
     vpc_security_group_ids    = ["sg-025028548d0e7a3d0"]   # Add your security group ID
@@ -15,33 +15,33 @@ resource "aws_instance" "ansible_server" {
   count                       = 1                          # Create a single instance
 
   tags = {
-    Name = "AnsibleServer"
+    Name = "AnsibleServer"                                 # Instance name
   }
 
   root_block_device {
-    volume_size = 50 
-    volume_type = "gp2" 
-    encrypted   = false                          # Unencrypt the root volume
+    volume_size = 50                                       # Root volume size in GB
+    volume_type = "gp2"                                    # General Purpose SSD
+    encrypted   = false                                    # Unencrypt the root volume
   }
 
   connection {
-    type        = "ssh"                          # Use SSH to connect to the instance
-    user        = "ec2-user"                     # Default user for Amazon Linux 2
-    private_key = file("/root/Khurram-key.pem")  # Path to your local PEM key
-    host        = self.public_ip                 # Public IP address of the instance
+    type        = "ssh"                                    # Use SSH to connect to the instance
+    user        = "ec2-user"                               # Default user for Amazon Linux 2
+    private_key = file("/root/Khurram-key.pem")            # Path to your local PEM key
+    host        = self.public_ip                           # Public IP address of the instance
   }
 
   provisioner "file" {
-    source      = "/root/Khurram-key.pem"           # Path to your local PEM key
-    destination = "/home/ec2-user/Khurram-key.pem"  # Destination path on the remote instance
+    source      = "/root/Khurram-key.pem"                 # Path to your local PEM key
+    destination = "/home/ec2-user/Khurram-key.pem"        # Destination path on the remote instance
   }
 
   provisioner "remote-exec" {
     inline = [
-      "sudo yum update -y",                           # Update the instance
-      "sudo amazon-linux-extras install ansible2 -y", # Install Ansible
-      "sudo yum install -y git python3 python3-pip",  # Install Git, Python3, and Pip3
-      "sudo pip3 install boto boto3 awscli",          # Install Boto, Boto3, and AWS CLI
+      "sudo yum update -y",                                           # Update the instance
+      "sudo amazon-linux-extras install ansible2 -y",                 # Install Ansible
+      "sudo yum install -y git python3 python3-pip",                  # Install Git, Python3, and Pip3
+      "sudo pip3 install boto boto3 awscli",                          # Install Boto, Boto3, and AWS CLI
       "sudo chown ec2-user:ec2-user /home/ec2-user/Khurram-key.pem",  # Change ownership of the PEM key
       "sudo chmod 400 /home/ec2-user/name.pem",                       # Change permissions of the PEM key
       "ssh-keygen -t rsa -N '' -f /home/ec2-user/.ssh/id_rsa",        # Generate SSH key pair
@@ -53,5 +53,5 @@ resource "aws_instance" "ansible_server" {
 }
 
 output "ansible_server_ip" {
-  value = aws_instance.ansible_server[0].public_ip # Output the public IP address of the instance
+  value = aws_instance.ansible_server[0].public_ip                                 # Output the public IP address of the instance
 }
