08f641fee79a0064b4648a14cb4392b2569c023c
Your Name <khurram539@hotmail.com>
2024-10-09 16:54:30 -0400

    change the cronical time

--- AMI/main.py
+++ AMI/main.py
@@ -1,14 +1,21 @@
 import boto3
+from datetime import datetime
 
 def list_amis():
-	# Create an EC2 resource
-	ec2 = boto3.resource('ec2')
+    # Create an EC2 resource
+    ec2 = boto3.resource('ec2')
 
-	# List all AMIs
-	images = ec2.images.filter(Owners=['self'])  # Use 'self' to list AMIs owned by you; remove filter to list all available AMIs
+    # List all AMIs
+    images = ec2.images.filter(Owners=['self'])  # Use 'self' to list AMIs owned by you; remove filter to list all available AMIs
 
-	for image in images:
-		print(f"ID: {image.id}, Name: {image.name}, Status: {image.state}")
+    # Fetch AMIs with their creation dates
+    amis = [(image.name, image.id, image.creation_date.split('T')[0], image.state) for image in images]
+
+    # Sort AMIs by creation date in descending order
+    sorted_amis = sorted(amis, key=lambda x: x[2], reverse=True)
+
+    for ami in sorted_amis:
+        print(f"Name: {ami[0]}, ID: {ami[1]}, Creation Date: {ami[2]}, Status: {ami[3]}")
 
 if __name__ == "__main__":
-	list_amis()
+    list_amis()
